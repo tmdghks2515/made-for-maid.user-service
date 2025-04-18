05ee016cd388dd7ea5229ea03d2dc8859b125ffc
ish <tmdghks0615@gmail.com>
2025-04-18 18:30:27 +0900

    GlobalExceptionHandler 추가

--- src/main/kotlin/io/madeformaid/user/UserServiceApplication.kt
+++ src/main/kotlin/io/madeformaid/user/UserServiceApplication.kt
@@ -5,12 +5,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import io.madeformaid.shared.config.AuthProperties
 import io.madeformaid.shared.context.EnableAuthContext
+import io.madeformaid.shared.exception.EnableGlobalExceptionHandling
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @EnableAuthContext
-@SpringBootApplication
+@EnableGlobalExceptionHandling
 @EnableConfigurationProperties(AuthProperties::class)
+@SpringBootApplication
 class UserServiceApplication
 
 fun main(args: Array<String>) {
