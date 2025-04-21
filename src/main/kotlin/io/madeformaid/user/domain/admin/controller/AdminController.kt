bb4b8c5ed04fa7db03e84a59d5ea2ee159e9735a
ish <tmdghks0615@gmail.com>
2025-04-21 17:44:39 +0900

    관리자 프로필 선택 API 추가

--- src/main/kotlin/io/madeformaid/user/domain/admin/controller/AdminController.kt
+++ src/main/kotlin/io/madeformaid/user/domain/admin/controller/AdminController.kt
@@ -1,9 +1,16 @@
 package io.madeformaid.user.domain.admin.controller
 
 import io.madeformaid.shared.context.AuthContext
+import io.madeformaid.user.domain.admin.dto.data.AdminProfileDTO
+import io.madeformaid.user.domain.admin.dto.data.AdminSignInResDTO
 import io.madeformaid.user.domain.admin.service.AdminService
+import io.madeformaid.user.utils.CookieProvider
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,9 +18,25 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/user/admin")
 class AdminController(
         private val adminService: AdminService,
+        private val cookieProvider: CookieProvider
 ) {
     @GetMapping("/profiles")
-    fun getProfiles(): ResponseEntity<Any> {
+    fun getProfiles(): ResponseEntity<List<AdminProfileDTO>> {
         return ResponseEntity.ok(adminService.getAdminProfiles(AuthContext.getAccountId()))
+    }
+
+    @PostMapping("/profile/{userId}")
+    fun selectProfile(
+            @PathVariable userId: String,
+            response: HttpServletResponse
+    ): ResponseEntity<AdminSignInResDTO> {
+        val (signInResponse, refreshToken) = adminService.selectProfile(userId)
+
+        response.setHeader(
+                HttpHeaders.SET_COOKIE,
+                cookieProvider.createRefreshTokenCookie(refreshToken).toString()
+        )
+
+        return ResponseEntity.ok(signInResponse)
     }
 }
--- src/main/kotlin/io/madeformaid/user/domain/admin/service/AdminService.kt
+++ src/main/kotlin/io/madeformaid/user/domain/admin/service/AdminService.kt
@@ -1,9 +1,15 @@
 package io.madeformaid.user.domain.admin.service
 
+import io.madeformaid.shared.context.AuthContext
+import io.madeformaid.shared.exception.BusinessException
+import io.madeformaid.shared.exception.ErrorCode
 import io.madeformaid.user.domain.admin.dto.data.AdminProfileDTO
+import io.madeformaid.user.domain.admin.dto.data.AdminSignInResDTO
 import io.madeformaid.user.domain.admin.mapper.AdminMapper
 import io.madeformaid.user.domain.user.repository.AccountRepository
 import io.madeformaid.user.domain.user.repository.UserRepository
+import io.madeformaid.user.utils.JwtTokenProvider
+import io.madeformaid.user.vo.SignInResStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,10 +19,28 @@ class AdminService(
         private val accountRepository: AccountRepository,
         private val userRepository: UserRepository,
         private val adminMapper: AdminMapper,
+        private val jwtTokenProvider: JwtTokenProvider,
 ) {
     @Transactional(readOnly = true)
     fun getAdminProfiles(accountId: String): List<AdminProfileDTO> {
         return userRepository.findAdminsByAccountId(accountId)
                 .map { adminMapper.toAdminProfileDTO(it) }
+    }
+
+    fun selectProfile(userId: String): Pair<AdminSignInResDTO, String> {
+        val account = accountRepository.findById(AuthContext.getAccountId())
+                .orElseThrow { BusinessException(ErrorCode.NOT_FOUND) }
+        val admin = account.users.find { it.id == userId } ?: throw BusinessException(ErrorCode.NOT_FOUND)
+
+        checkNotNull(admin.approvedAt) { "승인되지 않은 프로필 입니다. 사장님에게 승인을 요청해주세요." }
+
+        val adminDTO = adminMapper.toAdminDTO(admin)
+
+        return AdminSignInResDTO(
+                status = SignInResStatus.SIGN_IN_SUCCESS,
+                accountId = account.id,
+                accessToken = jwtTokenProvider.createAccessToken(adminDTO),
+                admin = adminDTO,
+        ) to jwtTokenProvider.createRefreshToken(adminDTO)
     }
 }
